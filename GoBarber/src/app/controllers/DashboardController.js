3c78bfb28da0f96983aa7a4c8ae3ef2fcb757dff
Caio Fernandes <caiovf13@gmail.com>
2020-02-01 10:20:27 -0300

    Implements different dashboard screen for  provider/user

--- GoBarber/src/app/controllers/DashboardController.js
+++ GoBarber/src/app/controllers/DashboardController.js
@@ -1,11 +1,38 @@
-const { User } = require('../models')
-
+const { User, Appointment } = require('../models')
+const moment = require('moment')
+const { Op } = require('sequelize')
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
+    if (!res.locals.user.provider) {
+      const providers = await User.findAll({ where: { provider: true } })
+      return res.render('dashboard', { providers })
+    }
+    // const { id } = req.session.user
 
-    return res.render('dashboard', { providers })
+    const appointments = await Appointment.findAll({
+      include: [{ model: User, as: 'user_id' }],
+      where: {
+        provider_id: req.session.user.id,
+        date: {
+          [Op.between]: [
+            moment()
+              .startOf('day')
+              .format(),
+            moment()
+              .endOf('day')
+              .format()
+          ]
+        }
+      }
+    })
+
+    // const appointments = await Appointment.findAll({
+    //   include: [{ model: User, as: 'user_id' }],
+    //   where: {
+    //     provider_id: id
+    //   }
+    // })
+    return res.render('providerDashboard', { appointments })
   }
 }
-
 module.exports = new DashboardController()
