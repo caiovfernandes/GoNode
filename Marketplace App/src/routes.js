8b9d925518d36bbf19685898b962c58172ac4d40
Caio Fernandes <caiovf13@gmail.com>
2020-03-07 08:31:14 -0300

    Add require-dir node module to controllers directory

--- Marketplace App/src/routes.js
+++ Marketplace App/src/routes.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const routes = express.Router()
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
 const authMiddleware = require('./app/middlewares/auth')
+const controllers = require('./app/controllers')
 
-routes.post('/users', UserController.store)
-routes.post('/session', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/session', controllers.SessionController.store)
 routes.get('/tokenTeste', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
